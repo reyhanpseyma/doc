7eef7ca9a0d14ebe64629618789fd7bec1c5ec49
Şeyma Paşalıoğlu <147440880+reyhanpseyma@users.noreply.github.com>
2024-05-21 23:59:52 +0300

    doc js
    navbar kullanarak açma ve kapama butonları oluşturduk

---
+++ jsödev/style.js
@@ -0,0 +1,8 @@
+function openNav() {
+  document.getElementById("sideNav").style.width = "300px";
+  document.getElementById("main").style.marginLeft = "300px";
+}
+function closeNav() {
+  document.getElementById("sideNav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
